faf34bd2efd1781a8659fae1f74daa77d1ca18b6
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-09 21:31:15 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int l, r, k;
+		cin >> l >> r >> k;
+		// just imagine a number line
+		// look at the image in the repository for better understanding
+		if (l == r) {
+			// if l == r, then there are no infinite number of distinct values between the given interval
+			// just print 1
+			cout << 1;
+		} else {
+			// if l != rm then there are infinite number of distinct values between the given interval
+			cout << k;
+		}
+		cout << '\n';
+	}
+	return 0;
+}
